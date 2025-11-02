d6d0c980e56f0d265665921a004f3f862aaeffd8
GitIzzy72 <isaacebencorn.uni@gmail.com>
2025-11-02 05:19:15 -0500

    some flask functionality

---
+++ assemble.py
@@ -0,0 +1,128 @@
+
+import os
+from instructor import patch
+from openai import OpenAI
+from flask import Flask, Request, json, jsonify, request
+
+from models import *
+
+app = Flask(__name__)
+
+client=OpenAI(base_url="https://openrouter.ai/api/v1",api_key=os.getenv('OPENROUTER_API_KEY'))
+modelCode = "google/gemini-2.5-flash"
+
+userData=''
+stateData='Maryland'
+countyData='Baltimore County'
+userInterests=['environmental discrimination','public transportation']
+newline="\n"
+currentGoals=[]
+currentTactics=[]
+
+@app.post('/location')
+def post_location():
+    check = check_params(request,['uid','state','county'])
+    valid = check[0]
+    data = check[1]
+
+    if(not valid):
+        return data,400
+
+    user = data['uid']
+    state = data['state']
+    county = data['county']
+
+    userData=user
+    stateData = state
+    countyData = county
+
+    return "Success",200
+
+@app.post('/interests')
+def post_interests():
+    check=check_params(request,['uid','interests'])
+    if(not check[0]):
+        return check[1],400
+    request_data = check[1]
+    user = request_data['uid']
+    interests = request_data['interests']
+    
+
+@app.get('/matchList')
+def get_goals_and_tactics():
+    """
+    Asks gemini for a list of relevant goals and tactics
+    """
+    uid=request.args['uid']
+    # Ask for a list of goals relevant to user's interests and their county
+    goalPrompt = f"""You are creating a matching game to match goals to relevant tactics. 
+    Provide a list of 5-6 goals related to the user\'s interests and community.\nThe user lives in {countyData},{stateData}.
+    \nInterests:\n {newline.join(f"* {x}" for x in userInterests)} \n Please respond in json format"""
+    
+    goalResponse = client.chat.completions.create(
+        model=modelCode,
+        messages=[
+            getSysPrompt(),
+            {
+                'role':'user',
+                'content':goalPrompt            
+            }
+        ],
+        response_format={
+            "type":"json_schema",
+            "json_schema": { 
+                "name":"GoalSchema",
+                "strict":True,
+                "schema":GoalResponse.model_json_schema()
+                }
+        }
+    )
+
+    goals= json.loads(json.dumps(goalResponse.choices[0].message.content))
+
+    #Send that list of goals back and ask for a list of tasks for matching 
+
+    tacticPrompt = f"""You are creating a matching game to match goals to relevant tactics. 
+                    Given the goals just created, make 3-4 tactics that can be used to advance these goals.
+                    The goals are: {newline.join(f"* {x}" for x in goals)} \n Each goal should have at least 1 tactic
+                    that is a strong pair. The user lives in {countyData}, {stateData}. Please respond in json format."""
+
+    print(tacticPrompt)
+
+    tacticResponse = client.chat.completions.create(
+        model=modelCode,
+        messages=[
+            getSysPrompt(),
+            {
+                'role':'user',
+                'content':tacticPrompt            
+            }
+        ],
+        response_format={
+            "type":"json_schema",
+            "json_schema": { 
+                "name":"TacticSchema",
+                "strict":True,
+                "schema":tacticResponse.model_json_schema()
+                }
+        }
+    )
+    
+    tactics = json.loads(json.dumps(tacticResponse.choices[0].message.content))
+    ##Return both the list of goals and tasks 
+    returnBody=jsonify({'goals':goals, 'tactics':tactics})
+
+    return returnBody
+
+def check_params(request: Request, params: list[str]):
+    requestVals = dict()
+    for x in params:
+        if(not x in request):
+            return (False,"Bad request - missing "+x)
+        else:
+            requestVals[x]=request[x]
+    return (True,requestVals)
+
+if __name__=='__main__':
+    app.run()
+    
---
+++ models.py
@@ -0,0 +1,53 @@
+from pydantic import BaseModel, Field
+
+MAX_GOALS = 6
+
+def getSysPrompt():
+    return { 'role':'system', 'content': "You are an assistant who helps educate users on effective ways to engage in activism and organize within their community. " \
+    "You're responses should stay relevant to the community of the current user."}
+    "You can give safe and effective advice using https://activisthandbook.org/ and specifically https://activisthandbook.org/strategy" 
+
+class Goal(BaseModel):
+    id :int =Field(description="unique numerical identifier")
+    title:str = Field(description="Name of the change the user would like to see in their community. This is a sentence fragment")
+    description : str = Field(description="Brief description of the issue, who it affects, why its important . Maximum 3 sentences.")
+    relation : str = Field(description="Explain how this connects to the user's community. Maximum 3 sentences")
+
+
+class GoalResponse(BaseModel):
+    goals: list[Goal] = Field(description="List of the requested goals", max_length=MAX_GOALS)
+
+
+class Tactic(BaseModel):
+    id :int =Field(description="unique numerical identifier")
+    name:str = Field(description="Name of method of activism or organization")
+    description : str = Field(description="1-2 sentences explaining what actions this tactic entails")
+
+class TaskResponse(BaseModel):
+    goals: list[Tactic] = Field(description="List of the requested tactics", max_length=MAX_GOALS-1)
+
+class Match(BaseModel):
+    goal: Goal = Field(description="The objective/cause that the user has paired with a tactic")
+    tactic: Tactic = Field(description="A tactic that the user beleives would be effective in furthering the selected goal")
+    compatability:int=Field(description="A rating 1-100 that describes how effective the tactic would be to further the goal. A score closer to 100 means the tactic is agood match. A score closer to 1 means employing the tactic would do almost nothing to further the goal.", ge=1, le=100)
+    explanation:str = Field(description="A 4-5 sentences to explain the reasoning behind the compatibility score. Including one example, modern or historical.")
+
+"""
+Goal:
+Name
+Brief description - max 250 chars
+Relation to the area - string 
+
+Tactic
+Name
+Brief description - max 250 chars
+GoalCompatibility - dict[goalName, compatibilityScore]
+
+Pairing
+Goal
+Tactic
+Compatibility - int 1 to 100
+Explanation - String - Description: This a few sentences to explain the reasoning behind their compatibility score, including one example, modern or historical.
+Sources
+
+"""
